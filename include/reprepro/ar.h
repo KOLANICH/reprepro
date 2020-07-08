d2b9cbfb7e72fc146a84f2f470c8ee7c9d244914
KOLANICH <kolan_n@mail.ru>
2020-07-08 12:11:33 +0300

    Refactoring the repo structure

---
+++ include/reprepro/ar.h
@@ -0,0 +1,22 @@
+#ifndef DEBCOMP_AR_H
+#define DEBCOMP_AR_H
+
+struct ar_archive;
+
+retvalue ar_open(/*@out@*/struct ar_archive **, const char *, InterruptedFT interrupted);
+void ar_close(/*@only@*/struct ar_archive *);
+
+/* RET_OK = next is there, RET_NOTHING = eof, < 0 = error */
+retvalue ar_nextmember(struct ar_archive *, /*@out@*/char ** /*filename*/, InterruptedFT interrupted);
+
+/* set compression for the next member */
+void ar_archivemember_setcompression(struct ar_archive *, enum compression);
+
+/* the following can be used for libarchive to read an file in the ar
+ * after ar_nextmember returned successfully.
+ * All references get invalid after the ar_nextmember is called again.  */
+int ar_archivemember_close(struct archive *, void *, InterruptedFT interrupted);
+int ar_archivemember_open(struct archive *, void *, InterruptedFT interrupted);
+ssize_t ar_archivemember_read(struct archive *, void *, const void **, InterruptedFT interrupted);
+
+#endif
